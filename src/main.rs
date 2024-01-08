926e64e7486bb3d84a3ba80a2ccc871b88fed817
cat_in_136 <cat.in.136+github@gmail.com>
2024-01-08 14:29:27 +0900

    fix codes

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,8 @@ use esp_idf_svc::{
     wifi::{AsyncWifi, EspWifi},
 };
 use futures::executor::block_on;
-use lazy_static::lazy_static;
 use log::info;
 use parking_lot::Mutex;
-use smart_leds::SmartLedsWrite;
 use std::{str, thread::sleep, time::Duration};
 use types::Pixel;
 use ws2812_esp32_rmt_driver::Ws2812Esp32Rmt;
@@ -33,22 +31,22 @@ const _FRONTEND_URL: &str = "http://192.168.194.11:3000";
 
 const NUM_LEDS: usize = 100;
 
-lazy_static! {
-    pub static ref NEO_PIXELS: Mutex<Ws2812Esp32Rmt<'static>> = Mutex::new(
-        Ws2812Esp32Rmt::new(
-            Peripherals::take().unwrap().rmt.channel0,
-            Peripherals::take().unwrap().pins.gpio17
-        )
-        .unwrap()
-    );
-    pub static ref LED_DATA: Mutex<[Pixel; NUM_LEDS]> = Mutex::new(generate_full_array(0, 0, 0));
-}
-
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
     EspLogger::initialize_default();
 
-    write_led_data()?;
+    let peripherals = Peripherals::take()?;
+    let mut neo_pixel = Mutex::new(Ws2812Esp32Rmt::new(
+        peripherals.rmt.channel0,
+        peripherals.pins.gpio17,
+    )?);
+    let led_data = Mutex::new(
+        (0..NUM_LEDS)
+            .map(|_| Pixel::new(0, 0, 0))
+            .collect::<Vec<_>>(),
+    );
+
+    write_led_data(&mut neo_pixel, &led_data)?;
 
     ThreadSpawnConfiguration {
         name: Some(b"LED-THREAD\0"),
@@ -60,24 +58,18 @@ fn main() -> anyhow::Result<()> {
     }
     .set()?;
 
-    std::thread::spawn(|| -> anyhow::Result<()> {
+    std::thread::spawn(move || -> anyhow::Result<()> {
         loop {
-            let data = *LED_DATA.lock();
-            unsafe { LED_DATA.force_unlock() };
-            *LED_DATA.lock() = data.map(|mut pixel| {
+            for pixel in &mut *led_data.lock() {
                 pixel.rainbow_tick();
-                pixel
-            });
+            }
 
-            write_led_data()?;
+            write_led_data(&mut neo_pixel, &led_data)?;
 
             sleep(Duration::from_millis(50));
         }
     });
 
-    ThreadSpawnConfiguration::default().set()?;
-
-    let peripherals = Peripherals::take().unwrap();
     let sys_loop = EspSystemEventLoop::take()?;
     let nvs = EspDefaultNvsPartition::take()?;
     let timer_service = EspTaskTimerService::new()?;
@@ -101,17 +93,15 @@ fn prevent_return() -> ! {
     }
 }
 
-fn write_led_data() -> anyhow::Result<()> {
-    NEO_PIXELS.lock().write(LED_DATA.lock().iter().cloned())?;
+fn write_led_data(
+    neo_pixels: &mut Mutex<Ws2812Esp32Rmt>,
+    led_data: &Mutex<Vec<Pixel>>,
+) -> anyhow::Result<()> {
+    let led_data_vec = led_data.lock();
+    neo_pixels
+        .lock()
+        .write_nocopy(led_data_vec.iter().map(|&v| rgb::RGB::<u8>::from(v)))?;
     Ok(())
-}
-
-fn generate_full_array(red: u8, green: u8, blue: u8) -> [Pixel; NUM_LEDS] {
-    [Pixel {
-        r: red,
-        g: green,
-        b: blue,
-    }; NUM_LEDS]
 }
 
 async fn connect_wifi(wifi: &mut AsyncWifi<EspWifi<'static>>) -> anyhow::Result<()> {
